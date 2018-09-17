efc56aa7825bcc37aa58525ea9a205fa1cde2932
Chang Liu <cliu@adobe.com>
2018-09-17 14:27:33 -0700

    Use pretrain resnet50 on JPG_chang rather than imagenet

--- keras_frcnn/resnet.py
+++ keras_frcnn/resnet.py
@@ -20,7 +20,8 @@ def get_weight_path():
     if K.image_dim_ordering() == 'th':
         return 'resnet50_weights_th_dim_ordering_th_kernels_notop.h5'
     else:
-        return 'resnet50_weights_tf_dim_ordering_tf_kernels.h5'
+        #return 'resnet50_weights_tf_dim_ordering_tf_kernels.h5'
+        return 'model_layer_recognition.134-0.558-0.9198.hdf5'
 
 def get_img_output_length(width, height):
     def get_output_length(input_length):
